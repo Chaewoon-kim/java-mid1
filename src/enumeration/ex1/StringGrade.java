ffe9e66a13be225eea1992f514ec964077f22ce2
Chaewoon-kim <bw06050@gmail.com>
2024-08-17 23:32:42 +0900

    eunmType 도입

---
+++ src/enumeration/ex1/StringGrade.java
@@ -0,0 +1,8 @@
+package enumeration.ex1;
+
+public class StringGrade {
+    //문자열 상수 사용 static final
+    public static final String BASIC = "BASIC";
+    public static final String GOLD = "GOLD";
+    public static final String DIAMOND = "DIAMOND";
+}
